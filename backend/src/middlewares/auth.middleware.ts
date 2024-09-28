4274dbf8340178be7cf88fd72a509fe1ac36cf6f
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-09-28 22:52:10 +0530

    create auth middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import {User} from '../models/user.model';
+import {Request, Response, NextFunction} from 'express';
+
+const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req?.cookies?.accessToken;
+
+  if (!token) {
+    return res.status(401).json({
+      status: 'error',
+      message: 'Access token and refresh token are required',
+    });
+  }
+
+  try {
+    const decodedAccessToken = jwt.verify(
+      token,
+      // @ts-ignore
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedAccessToken.id);
+
+    if (!user) {
+      return res.status(401).json({
+        status: 'error',
+        message: 'Invalid access token',
+      });
+    }
+
+    // @ts-ignore
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      status: 'error',
+      message: 'Invalid access token',
+    });
+  }
+};
+
+export default verifyJWT;
